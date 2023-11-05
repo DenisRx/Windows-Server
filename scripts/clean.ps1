1e78632b9ed7394f65b72f26d86ccc8a933eb017
Denis Roux <denis.roux@epitech.eu>
2023-11-05 23:31:33 +0100

    Add creation of all VMs in install script

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,1 +1,9 @@
+VBoxManage unregistervm "CA-AD" --delete
+VBoxManage unregistervm "DHCP-DNS" --delete
+VBoxManage unregistervm "SharePoint-DNS" --delete
 VBoxManage unregistervm "SQLServer" --delete
+
+Remove-Item "C:\Users\rouxd\VirtualBox VMs\CA-AD" -Recurse
+Remove-Item "C:\Users\rouxd\VirtualBox VMs\DHCP-DNS" -Recurse
+Remove-Item "C:\Users\rouxd\VirtualBox VMs\SharePoint-DNS" -Recurse
+Remove-Item "C:\Users\rouxd\VirtualBox VMs\SQLServer" -Recurse
--- scripts/install.ps1
+++ scripts/install.ps1
@@ -6,7 +6,8 @@ function CreateVm {
         [int]$MemoryCount,
         [int]$DiskSize,
         [string]$DiskPath,
-        [string]$IsoPath
+        [string]$IsoPath,
+        [int]$imageIndex
     )
 
     # Create the virtual machine
@@ -40,7 +41,7 @@ function CreateVm {
     VBoxManage modifyvm $VmName --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
     # Unattended installation of Windows server
-    VBoxManage unattended install $VmName --iso=$IsoPath --user=denis --password pass1234 --locale="en_US" --country="FR" --time-zone=CET --hostname="${VmName}.ws2-2324-denis.hogent" --image-index=1 --install-additions
+    VBoxManage unattended install $VmName --iso=$IsoPath --user=denis --password pass1234 --locale="en_US" --country="FR" --time-zone=CET --hostname="${VmName}.ws2-2324-denis.hogent" --image-index=$imageIndex --install-additions
 }
 
 function StartVm {
@@ -49,10 +50,21 @@ function StartVm {
     VBoxManage startvm $VmName --type headless
 }
 
-$VmName = "SQLServer"
-$OsType = "Windows2022_64"
-$DiskPath = "C:\Users\rouxd\VirtualBox VMs\${VmName}\${VmName}.vdi"
+$WSOsType = "Windows2022_64"
+$W10OsType = "Windows10_64"
 $WSIsoPath = "C:\Users\rouxd\OneDrive\Bureau\Windows-Server\images\en-us_windows_server_2022_x64.iso"
+$W10IsoPath = "C:\Users\rouxd\OneDrive\Bureau\Windows-Server\images\en-us_windows_10_x64.iso"
 
-CreateVm "SQLServer" $OsType 2 (4 * 1024) (64 * 1024) $DiskPath $WSIsoPath
-StartVm $VmName
+# Create VMs
+CreateVm "CA-AD" $WSOsType 1 (2 * 1024) (10 * 1024) "C:\Users\rouxd\VirtualBox VMs\CA-AD\CA-AD.vdi" $WSIsoPath 1
+CreateVm "DHCP-DNS" $WSOsType 1 (2 * 1024) (10 * 1024) "C:\Users\rouxd\VirtualBox VMs\DHCP-DNS\DHCP-DNS.vdi" $WSIsoPath 1
+CreateVm "SharePoint-DNS" $WSOsType 2 (4 * 1024) (45 * 1024) "C:\Users\rouxd\VirtualBox VMs\SharePoint-DNS\SharePoint-DNS.vdi" $WSIsoPath 2
+CreateVm "SQLServer" $WSOsType 2 (4 * 1024) (64 * 1024) "C:\Users\rouxd\VirtualBox VMs\SQLServer\SQLServer.vdi" $WSIsoPath 1
+CreateVm "Client" $W10OsType 2 (4 * 1024) (35 * 1024) "C:\Users\rouxd\VirtualBox VMs\Client\Client.vdi" $W10IsoPath 1
+
+# Start VMs
+StartVm CA-AD
+StartVm DHCP-DNS
+StartVm SharePoint-DNS
+StartVm SQLServer
+StartVm Client
