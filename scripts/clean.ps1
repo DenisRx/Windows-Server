8325e5c8d702e646177852b134ce980c5b0ff633
Denis Roux <denis.roux@epitech.eu>
2023-12-12 17:11:30 +0100

    Rename client machine

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,9 +1,9 @@
 VBoxManage unregistervm "Domain" --delete
 VBoxManage unregistervm "SharePoint" --delete
 VBoxManage unregistervm "SQLServer-DNS" --delete
-VBoxManage unregistervm "Client" --delete
+VBoxManage unregistervm "Client1" --delete
 
 Remove-Item "C:\Users\rouxd\VirtualBox VMs\Domain" -Recurse
 Remove-Item "C:\Users\rouxd\VirtualBox VMs\SharePoint" -Recurse
 Remove-Item "C:\Users\rouxd\VirtualBox VMs\SQLServer-DNS" -Recurse
-Remove-Item "C:\Users\rouxd\VirtualBox VMs\Client" -Recurse
+Remove-Item "C:\Users\rouxd\VirtualBox VMs\Client1" -Recurse
