7de6244fb62f9ab127dbe166bae2d10ad9f6d42e
Denis Roux <denis.roux@epitech.eu>
2023-12-12 18:27:07 +0100

    Add DNS ip for client

--- scripts/Client1/client1.ps1
+++ scripts/Client1/client1.ps1
@@ -1,5 +1,9 @@
+# Set the DNS IP address
+Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses 192.168.23.5
+
 $username = "denis"
 $password = ConvertTo-SecureString "pass1234" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
+# Add client computer to the domain
 Add-Computer -ComputerName "Client1" -DomainName "WS2-2324-denis.hogent" -Credential $credential
