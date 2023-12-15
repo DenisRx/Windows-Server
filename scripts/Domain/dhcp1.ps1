f878ba1b033fefdba1308dc89df61c96a20acd1d
Denis Roux <denis.roux@epitech.eu>
2023-12-15 15:38:56 +0100

    Add user to enterprise admins domain group

--- scripts/Domain/dhcp1.ps1
+++ scripts/Domain/dhcp1.ps1
@@ -3,3 +3,6 @@ Install-WindowsFeature -Name DHCP -IncludeManagementTools
 
 # Configure DHCP Administrators and DHCP Users
 Add-DhcpServerSecurityGroup
+
+# Add the user to Enterprise Admins domain group
+Add-ADGroupMember -Identity "Enterprise Admins" -Members "denis"
--- scripts/Domain/dhcp2.ps1
+++ scripts/Domain/dhcp2.ps1
@@ -1,14 +1,16 @@
 # Authorize DHCP Server in AD DS
 Add-DhcpServerInDC -DnsName "Domain.WS2-2324-denis.hogent" -IPAddress 192.168.23.5
 
-Add-DhcpServerv4Scope -Name "dhcp.ws2-2324-denis.hogent" -StartRange 192.168.23.51 -EndRange 192.168.23.100 -SubnetMask 255.255.255.0 -LeaseDuration 8.00:00:00
-
-Set-DhcpServerv4OptionValue -DnsDomain "WS2-2324-denis.hogent" -DnsServer 192.168.23.5 -Router 192.168.23.2
-
 # Finish DHCP configuration
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\ServerManager\Roles\12" -Name ConfigurationState -Value 2
+
+# Define the range of possible IP addresses to provide and the validity duration
+Add-DhcpServerv4Scope -Name "dhcp.ws2-2324-denis.hogent" -StartRange 192.168.23.51 -EndRange 192.168.23.100 -SubnetMask 255.255.255.0 -LeaseDuration 8.00:00:00 -State InActive
+
+# Configure DNS Server for DHCP
+Set-DhcpServerv4OptionValue -DnsDomain "WS2-2324-denis.hogent" -DnsServer 192.168.23.5 -Router 192.168.23.2
 
 # Activate DHCP scope
 Set-DhcpServerv4Scope -ScopeId 192.168.23.0 -State Active
 
-Start-Service DHCPserver
+Restart-Service DHCPserver -Force
